745b135c059a3a551698002373c55f7294612689
Daiane Wan-Dall <dwandall@gmail.com>
2023-04-19 15:03:09 -0300

    Esboço do projeto

---
+++ visor.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Login de Usuário</title>
+
+</head>
+<body>
+    <div>
+        <header>
+
+        </header>
+    </div>
+    <div>
+        <table>
+            <thead>
+                Recepção
+            </thead>
+            <tbody>
+                <tr>02</tr>
+            </tbody>
+        </table>
+    </div>
+    <div>
+
+    </div>
+</body>
+</html>
